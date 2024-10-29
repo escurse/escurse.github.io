601d1ad880e74b5336f39e9778378f8235d4ba08
escurse <av1967@naver.com>
2024-10-29 16:02:34 +0900

    push

---
+++ assets/scripts/index.js
@@ -0,0 +1,20 @@
+const $form = document.getElementById('form');
+const $div = document.createElement('div');
+
+$form.onsubmit = (e) => {
+    e.preventDefault();
+    const $valA = $form['valA'].value;
+    const $valB = $form['valB'].value;
+    if ($form['op'].value === "add") {
+        $div.innerText = parseInt($valA) + parseInt($valB);
+    } else if ($form['op'].value === "sub") {
+        $div.innerText = parseInt($valA) - parseInt($valB);
+    } else if ($form['op'].value === "mul") {
+        $div.innerText = parseInt($valA) * parseInt($valB);
+    } else if ($form['op'].value === "div") {
+        $div.innerText = parseInt($valA) / parseInt($valB);
+    } else if ($form['op'].value === "mod") {
+        $div.innerText = parseInt($valA) % parseInt($valB);
+    }
+    $form.append($div);
+}
